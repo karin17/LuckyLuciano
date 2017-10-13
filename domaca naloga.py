192d97e782e4d0466b054e2d6f73e157ad006bf4
karin jurse <karin.jurse@gmail.com>
2017-10-13 16:59:16 +0200

    prvi commit

---
+++ domaca naloga.py
@@ -0,0 +1,8 @@
+
+secret = 7
+guess = int (raw_input ("enter the secret value: "))
+print guess
+if guess == "secret":
+    print "Congratulation. You are really Lucky :)"
+else:
+    print "Maybe you should take a break from gambling ;)"
